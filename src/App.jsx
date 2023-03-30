e1441f11757d86bbb7319bbd7cdb8017ffa38e58
himarnoel <olaimarnoel@gmail.com>
2023-03-30 02:24:49 +0100

    started project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Land from "./Auth/Land";
+import Login from "./Auth/Login";
+import Signup from "./Auth/Signup";
+import Search from "./App/Search";
+import Detail from "./App/Detail";
+
+function App() {
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Land />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Auth/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signup = () => {
+  return (
+    <div>
+      <div className="w-full bg-[#]">dsfadf</div>
+    </div>
+  );
+};
+
+export default Signup;
