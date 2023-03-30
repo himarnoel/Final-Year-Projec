4c2410781c1e53b5d3225ea880f7311794975848
himarnoel <olaimarnoel@gmail.com>
2023-03-30 08:56:51 +0100

    done

--- src/Auth/Land.jsx
+++ src/Auth/Land.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import logo from "../assets/Logo.png";
 import app from "../assets/app.svg";
+import { useNavigate } from "react-router-dom";
 const Land = () => {
+  const navigate = useNavigate();
   return (
     <div className="font-raleway flex h-screen flex-col ">
       <div className=" flex justify-between py-4 px-20">
         <img src={logo} alt="" className="object-contain w-[9rem]" />
         <div className="w-[18rem] flex items-center justify-between">
-          <button className="bg-[#007DFE] py-2 px-10 text-white rounded-lg font-medium">
+          <button
+            onClick={() => navigate("/signup")}
+            className="bg-[#007DFE] py-2 px-10 text-white rounded-lg font-medium"
+          >
             Signup
           </button>
-          <button className="border border-[#007DFE] text-[#007DFE] py-2 px-10 font-medium rounded-lg">
+          <button
+            onClick={() => navigate("/login")}
+            className="border border-[#007DFE] text-[#007DFE] py-2 px-10 font-medium rounded-lg"
+          >
             Login
           </button>
         </div>
@@ -20,8 +28,13 @@ const Land = () => {
           <p className="text-[#506269] text-5xl w-[23rem] leading-[1.2] font-semibold">
             We Check Out Apps before You use Them{" "}
           </p>
-          <p className="text-[#848484] text-xl font-thin mt-4">Android and iOS Apps</p>
-          <button className="py-4 px-24 text-white bg-[#007DFE] mt-4">
+          <p className="text-[#848484] text-xl font-thin mt-4">
+            Android and iOS Apps
+          </p>
+          <button
+            onClick={() => navigate("/")}
+            className="py-4 px-24 text-white bg-[#007DFE] mt-4"
+          >
             Check Apps
           </button>
         </div>
