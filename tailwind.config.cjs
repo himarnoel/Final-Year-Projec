9e561d890de0e90f70462528a1f5bbbf15cd4632
himarnoel <olaimarnoel@gmail.com>
2023-03-30 02:29:39 +0100

    adding fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    raleway: ["Raleway", ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
 }
