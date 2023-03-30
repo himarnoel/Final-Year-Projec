1d241c815988d813168e2c37ff3d555e0be9029d
himarnoel <olaimarnoel@gmail.com>
2023-03-30 03:18:36 +0100

    google

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -1,32 +1,17 @@
 import React from "react";
-
+import loginimg from "../assets/login.png";
 const Login = () => {
   return (
     <div>
       <div className="w-full h-screen bg-[#fff] font-raleway items-center gap-x-8 grid grid-cols-2">
         <div className="dd 3xl:bg-blue-100 flex h-full justify-center flex-col items-center ">
           <h1 className="text-[#2B2B2B] text-5xl font-semibold w-[22rem] text-center">
-            Welcome Here
+            Welcome Back
           </h1>
           <p className="text-[#2B2B2B80] text-sm mt-4">
             Please enter your contact details to connect.
           </p>
           <form action="" method="get">
-            <div className="mt-4">
-              <label
-                htmlFor=""
-                className="text-[#2B2B2B] text-sm block font-semibold ml-3"
-              >
-                Name
-              </label>
-              <input
-                id="name"
-                type="text"
-                className="mt-1 focus:outline-none  rounded-lg border border-[#D7D7D7] text-xs py-3 w-[22rem] px-2 placeholder:text-xs leading-tight focus:shadow-outline"
-                placeholder="John Doe"
-              />
-            </div>
-
             <div className="mt-8">
               <label
                 htmlFor=""
@@ -41,7 +26,7 @@ const Login = () => {
                 placeholder="name@compagny.com"
               />
             </div>
-            <div className="mt-8">
+            <div className="mt-4">
               <label
                 htmlFor=""
                 className="text-[#2B2B2B] text-sm block font-semibold ml-3"
@@ -55,7 +40,7 @@ const Login = () => {
                 placeholder="name@compagny.com"
               />
             </div>
-            <button className="bg-[#007DFE] w-[22rem] mt-10 rounded-lg py-2 text-white">
+            <button className="bg-[#007DFE] w-[22rem] mt-8 shadow-sm rounded-lg py-2 text-white">
               Signup
             </button>
             <p className="w-[22rem] text-xs justify-center mt-4 flex">
@@ -65,7 +50,7 @@ const Login = () => {
           </form>
         </div>
         <div className="dd 3xl:bg-rose-200 flex items-center  justify-center h-full">
-          <img src={signupimg} alt="" className="object-contain w-[38rem]" />
+          <img src={loginimg} alt="" className="object-contain w-[38rem]" />
         </div>
       </div>
     </div>
