22c54b56cd4b302f547530cae43c0ad89572585a
himarnoel <olaimarnoel@gmail.com>
2023-04-04 01:33:52 +0100

    adjusting images

--- src/App/Home.jsx
+++ src/App/Home.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import logo from "../assets/Logo.png";
+import bg from "../assets/bg.png";
+import apps from "../assets/apps.png";
 const Home = () => {
   return (
     <div>
-      <div className=" flex justify-between py-4 px-20">
+      <div className=" flex justify-between py-4 px-20 shadow-md">
         <img src={logo} alt="" className="object-contain w-[9rem]" />
-        <div className="w-[18rem] flex items-center justify-between">
-          <button
-            onClick={() => navigate("/signup")}
-            className="bg-[#007DFE] py-2 px-10 text-white rounded-lg font-medium"
-          >
-            Signup
-          </button>
-          <button
-            onClick={() => navigate("/login")}
-            className="border border-[#007DFE] text-[#007DFE] py-2 px-10 font-medium rounded-lg"
-          >
-            Login
-          </button>
-        </div>
+        <div className="w-[18rem] flex items-center justify-between"></div>
+      </div>
+      <div className="relative ">
+        {" "}
+        <img src={bg} className="absolute" />
+        <img src={apps} className="absolute right-0 w-[20rem] object-contain" />
       </div>
     </div>
   );
