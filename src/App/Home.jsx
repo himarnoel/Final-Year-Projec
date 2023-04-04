11f1a0cf37111d8d1e962289aa73215743931823
himarnoel <olaimarnoel@gmail.com>
2023-04-04 01:18:21 +0100

    working on home.jsx

--- src/App/Home.jsx
+++ src/App/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div className="full"></div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
