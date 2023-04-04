078fd610f7860a6e24c75537e970ff8ecb597f75
himarnoel <olaimarnoel@gmail.com>
2023-04-04 02:29:53 +0100

    added avatar image

--- src/App/Home.jsx
+++ src/App/Home.jsx
@@ -14,11 +14,13 @@ import img9 from "../assets/apps/image9.png";
 const Home = () => {
   return (
     <div className="font-raleway">
-      <div className=" flex justify-between py-4 px-20 shadow-md fixed top-0 z-[20] w-full  bg-white">
+      <div className=" flex justify-between py-4 px-20 shadow-md fixed top-0 z-[20] w-full  bg-black/[0.8]">
         <img src={logo} alt="" className="object-contain w-[8rem]" />
-        <div className=""></div>
+        <div className="">
+            <img src="" alt="" />
+        </div>
       </div>
-      <div className="relative mt-[3.2rem] ">
+      <div className="relative mt-[3.1rem] ">
         {" "}
         <img src={bg} className="absolute" />
         <p className="absolute text-white left-20 inset-16 font-semibold text-2xl ">
@@ -26,7 +28,7 @@ const Home = () => {
         </p>
         <img
           src={apps}
-          className="absolute z-[-1] right-0 w-[41.3rem] object-contain"
+          className="absolute z-[-1] right-0 w-[41rem] object-contain"
         />
       </div>
       <div className="w-[70rem] mx-auto mt-64 left-0 right-0 absolute  px-20  grid grid-cols-4 gap-y-10 justify-items-center">
