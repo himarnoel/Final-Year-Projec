7382445758fa75ebef294639744693a21d97d32f
himarnoel <olaimarnoel@gmail.com>
2023-04-04 02:17:16 +0100

    working on home page

--- src/App/Home.jsx
+++ src/App/Home.jsx
@@ -14,11 +14,11 @@ import img9 from "../assets/apps/image9.png";
 const Home = () => {
   return (
     <div className="font-raleway">
-      <div className=" flex justify-between py-4 px-20 shadow-md fixed top-0 ">
+      <div className=" flex justify-between py-4 px-20 shadow-md fixed top-0 z-[20] w-full  bg-white">
         <img src={logo} alt="" className="object-contain w-[8rem]" />
         <div className=""></div>
       </div>
-      <div className="relative ">
+      <div className="relative mt-[3.2rem] ">
         {" "}
         <img src={bg} className="absolute" />
         <p className="absolute text-white left-20 inset-16 font-semibold text-2xl ">
@@ -29,7 +29,7 @@ const Home = () => {
           className="absolute z-[-1] right-0 w-[41.3rem] object-contain"
         />
       </div>
-      <div className="w-[70rem] mx-auto   px-20 mt-[14rem] grid grid-cols-4 gap-y-10 justify-items-center">
+      <div className="w-[70rem] mx-auto mt-64 left-0 right-0 absolute  px-20  grid grid-cols-4 gap-y-10 justify-items-center">
         <div className="border-2 rounded-lg border-solid w-fit cursor-pointer">
           <img src={img1} alt="" className="p-4 object-contain" />
         </div>
