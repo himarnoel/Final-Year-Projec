e3a9cc37b0d0771e0069c428d9d6d615afda94e2
himarnoel <olaimarnoel@gmail.com>
2023-04-04 01:28:28 +0100

    added nav bar

--- src/App/Home.jsx
+++ src/App/Home.jsx
@@ -3,8 +3,22 @@ import logo from "../assets/Logo.png";
 const Home = () => {
   return (
     <div>
-      <div className="full">
+      <div className=" flex justify-between py-4 px-20">
         <img src={logo} alt="" className="object-contain w-[9rem]" />
+        <div className="w-[18rem] flex items-center justify-between">
+          <button
+            onClick={() => navigate("/signup")}
+            className="bg-[#007DFE] py-2 px-10 text-white rounded-lg font-medium"
+          >
+            Signup
+          </button>
+          <button
+            onClick={() => navigate("/login")}
+            className="border border-[#007DFE] text-[#007DFE] py-2 px-10 font-medium rounded-lg"
+          >
+            Login
+          </button>
+        </div>
       </div>
     </div>
   );
