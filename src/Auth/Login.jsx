0be94a360de3d0bebce43e25de43d8585e22cd09
himarnoel <olaimarnoel@gmail.com>
2023-04-20 00:02:45 +0100

    working on email

--- src/Auth/Login.jsx
+++ src/Auth/Login.jsx
@@ -2,10 +2,18 @@ import React from "react";
 import loginimg from "../assets/login.png";
 import google from "../assets/google.svg";
 import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
 
 const Login = () => {
   const navigate = useNavigate();
-
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: signupValidate,
+    onSubmit: (values) => {},
+  });
   return (
     <div>
       <div className="w-full h-screen bg-[#fff] font-raleway items-center gap-x-8 grid grid-cols-2">
@@ -29,6 +37,9 @@ const Login = () => {
                 type="text"
                 className="mt-1 focus:outline-none rounded-lg border focus:border-[#007DFE]  border-[#D7D7D7] text-xs py-3 w-[22rem] px-2 placeholder:text-xs leading-tight focus:shadow-outline shadow-sm"
                 placeholder="name@compagny.com"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                onBlur={formik.handleBlur}
               />
             </div>
             <div className="mt-4">
@@ -52,7 +63,7 @@ const Login = () => {
               onClick={() => navigate("/home")}
               className="bg-[#007DFE] font-semibold w-[22rem] text-sm mt-4 shadow-sm rounded-lg py-3 text-white"
             >
-           Login
+              Login
             </button>
             <button className=" border-[#D7D7D7] border text-sm flex items-center justify-center w-[22rem] mt-4 shadow-sm rounded-lg py-2 text-[2B2B2B]">
               <img src={google} alt="" />{" "}
